12463448a9db73f7accd1211800bc7270bc9147d
Aidan A <84807134+Aidan-A-2008@users.noreply.github.com>
2023-02-14 19:38:54 -0600

    Create speed-test.py

---
+++ speed-test.py
@@ -0,0 +1,20 @@
+# All th things needed to run this project. 
+## Don't remove.
+import speedtest
+from rich import print
+
+print("    ")
+print("[bold red]-------------------------------------------------------------")
+print("    ")
+print("[bold red]Keep in Mind, Internet speed chnage so much that these results may not be as accurate as a speed test site.")
+print("    ")
+print("[bold red]-------------------------------------------------------------")
+print("    ")
+print("[bold blue]Running SpeedTest")
+test = speedtest.Speedtest()
+download = test.download()
+upload = test.upload()
+print("[bold green]|----------------------------------|")
+print(f"[bold green]|Download Speed | {download}|")
+print(f"[bold green]|Upload Speed   | {upload}|")
+print("[bold green]|----------------------------------|")
