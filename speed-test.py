5817f5b075c62ab76f7fc545dd51665201dabf90
Aidan A <84807134+Aidan-A-2008@users.noreply.github.com>
2023-02-17 06:51:02 -0600

    Update speed-test.py

--- speed-test.py
+++ speed-test.py
@@ -1,8 +1,15 @@
 # All th things needed to run this project. 
-## Don't remove.
+## Don't remove. | Removing this will not allow the code to run and you will recive a long error regarding you removing this. 
 import speedtest
 from rich import print
+from tkinter import *
+from tkinter.ttk import *
 
+print("    ")
+print("[yellow]Python Speed Tester Beta 1.1.0")
+print("    ")
+print("    ")
+print("[purple]Beta Update: Added App/GUI | Test will show in both console/shell/terminal and via output/app/GUI")
 print("    ")
 print("[bold red]-------------------------------------------------------------")
 print("    ")
@@ -10,11 +17,55 @@ print("[bold red]Keep in Mind, Internet speed chnage so much that these results 
 print("    ")
 print("[bold red]-------------------------------------------------------------")
 print("    ")
-print("[bold blue]Running SpeedTest")
+print("[blue]Running SpeedTest")
+print("    ")
+
+#Declaring the varibles for the Speed Tester
 test = speedtest.Speedtest()
 download = test.download()
 upload = test.upload()
-print("[bold green]|----------------------------------|")
-print(f"[bold green]|Download Speed | {download}")
-print(f"[bold green]|Upload Speed   | {upload}")
-print("[bold green]|----------------------------------|")
+print(f"[green]Download Speed | {download}")
+print(f"[green]Upload Speed   | {upload}")
+
+# creates a Tk() object
+master = Tk()
+
+# sets the geometry of main
+# root window
+master.geometry("200x200")
+
+
+# function to open a new window
+# on a button click
+def openNewWindow():
+	
+	# Toplevel object which will
+	# be treated as a new window
+	newWindow = Toplevel(master)
+
+	# sets the title of the
+	# Toplevel widget
+	newWindow.title("Python Speed Test")
+
+	# sets the geometry of toplevel
+	newWindow.geometry("150x500")
+
+	# A Label widget to show in toplevel
+	Label(newWindow,
+		text ="Python Speed Test Beta 1.1.0").pack()
+
+
+label = Label(master,
+			text ="Python Speed Test V: Beta 1.1.0")
+
+label.pack(pady = 10)
+
+# a button widget which will open a
+# new window on button click
+btn = Button(master,
+			text ="Run Speed Test",
+			command = "python3 speed-test.py")
+btn.pack(pady = 10)
+
+# mainloop, runs infinitely
+mainloop()
